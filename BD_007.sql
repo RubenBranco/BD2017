80d9addbc435d2a14dd7553efad31028f9350ccf
RubenBranco <ruben.branco@outlook.pt>
2017-11-01 18:23:37 +0000

    Inicial

---
+++ BD_007.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Produto(
+  nome VARCHAR(80),
+  tipo VARCHAR(20),
+  designação genérica VARCHAR(30),
+  marca VARCHAR(30),
+  mercado VARCHAR(10),
+  numero identificação INT(10)
+);
+
+CREATE TABLE Empresa(
+  morada sede VARCHAR(100),
+  nif INT(9),
+  capital social INT(20)
+);
+
+CREATE TABLE Contactos ECOFCUL(
+
+);
